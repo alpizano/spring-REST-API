313a6f1197da3dc45409e810aa50370642f9608b
al <apizano@purdue.edu>
2020-04-08 21:34:17 -0500

    updating data object

--- rest-service/src/main/java/com/demo/restservice/Films.java
+++ rest-service/src/main/java/com/demo/restservice/Films.java
@@ -14,17 +14,27 @@ public class Films {
     private final String description;
     private final String director;
     private final String producer;
-    private final long date;
-    private final int score;
+    private final long release_date;
+    private final int rt_score;
+    private final String people;
+    private final String species;
+    private final String locations;
+    private final String url;
 
-    public Films(long id, String title, String description, String director, String producer, long date, int score) {
+    public Films(long id, String title, String description, String director, String producer, long release_date,
+                 int rt_score, String people, String species, String locations, String url) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.director = director;
         this.producer = producer;
-        this.date = date;
-        this.score = score;
+        this.release_date = release_date;
+        this.rt_score = rt_score;
+        this.people = people;
+        this.species = species;
+        this.locations = locations;
+        this.url = url;
+
     }
 
 }
