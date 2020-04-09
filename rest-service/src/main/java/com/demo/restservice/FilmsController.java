7c2d02128812d74aa04a96037935a106f71dbd59
al <apizano@purdue.edu>
2020-04-08 21:01:05 -0500

    adding JDBC depen for postgresql and adding controller

--- rest-service/src/main/java/com/demo/restservice/FilmsController.java
+++ rest-service/src/main/java/com/demo/restservice/FilmsController.java
@@ -1,4 +1,9 @@
 package com.demo.restservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class FilmsController {
+
+    @GetMapping("/films")
+    public Films film(@Request)
 }
